399cfb5b7221ebb6014440a2eb2187fc4b67a195
Mahyar Torkamanzadeh <mahyar.tz@gmail.com>
2023-11-11 08:11:45 +0330

    Initial commit 2

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/login',[\App\Http\Controllers\User\AuthController::class,'showLogin']);
+Route::get('/',[\App\Http\Controllers\User\AuthController::class,'showLogin']);
 Route::get('/register',[\App\Http\Controllers\User\AuthController::class,'showRegister']);
 Route::get('/', function () {
     return view('layouts.app');
